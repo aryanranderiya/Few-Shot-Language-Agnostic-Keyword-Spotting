a4c72d7da71acbf6c245f9678e329fc5e6398f07
Aryan Randeriya <aryanranderiya1478@gmail.com>
2024-09-04 20:29:58 +0530

    Can convert videos to audio

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,26 @@
-from typing import Union
-from fastapi import FastAPI
+from typing import Union, Annotated
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from functions.convert import to_audio
+import os
 
 app = FastAPI()
 
 
 @app.get("/")
-def read_root():
+def ping():
     return {"Hello": "World"}
+
+
+@app.post("/convert_audio")
+async def convert_audio(file: UploadFile):
+    file_location = f"{file.filename}"\
+
+    with open(file_location, "wb") as f:
+        f.write(await file.read())
+
+    result = await to_audio(file_location)
+
+    if "error" in result:
+        raise HTTPException(status_code=500, detail=result["error"])
+
+    return result
